c17cbff9f9c4b07d2538a2eb900c231be555e4f2
Ekantika K <ekantika.kri@gmail.com>
2025-03-31 12:51:12 +0530

    RAG script

--- RAG.py
+++ RAG.py
@@ -4,11 +4,12 @@ from langchain_huggingface import HuggingFaceEndpoint
 from langchain_core.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 
-#Step 1: Set up LLM (BioMistral with HF)
+
+#Step 1: Set up LLM (Mistral with HF)
 HF_TOKEN=os.environ.get("HF_TOKEN")
-HUGGINGFACE_REPO_ID="BioMistral/BioMistral-7B"
+HUGGINGFACE_REPO_ID="mistralai/Mistral-7B-Instruct-v0.3"
 
 def load_llm(huggingface_repo_id):
     llm=HuggingFaceEndpoint(
@@ -39,14 +40,14 @@ def set_custom_prompt(custom_prompt_template):
 
 # load database:
 DB_FAISS_PATH="vectorstore"
-embedding_model=HuggingFaceEmbeddings(model_name="") #not sure which model (if any at all) was used in the database creation
+embedding_model=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2") #embedding model
 db=FAISS.load_local(DB_FAISS_PATH, embedding_model, allow_dangerous_deserialization=True)
 
 # QA Chain
 qa_chain=RetrievalQA.from_chain_type(
     llm=load_llm(HUGGINGFACE_REPO_ID),
-    chain_type="stuff",   #concatenates all retrieved ans into a single prompt and ends everything to the LLM at once
-    retriever=db.as_retriever(search_kwargs={'k':5}),  #top 5 results
+    chain_type="stuff", 
+    retriever=db.as_retriever(search_kwargs={'k':3}), #top 3 results only
     return_source_documents=True,
     chain_type_kwargs={'prompt':set_custom_prompt(CUSTOM_PROMPT_TEMPLATE)}
 )
@@ -56,4 +57,3 @@ user_query=input("Write Query Here: ")
 response=qa_chain.invoke({'query': user_query})
 print("RESULT: ", response["result"])
 print("SOURCE DOCUMENTS: ", response["source_documents"])
-
